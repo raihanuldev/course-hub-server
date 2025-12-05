b2858c50a78d0d9243b893dbafdac09357e2cde6
raihanuldev <rihanthedev@gmail.com>
2025-12-05 15:23:19 +0600

    getUserbyEmail

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const app = require('./app')
 const { connectDb } = require('./src/config/db')
 const PORT = process.env.PORT || 5000
 
-connectDb();
+connectDb().then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => console.log('DB connection failed:', err));
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+// })
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,14 @@
+const { getDB } = require("../config/db");
+const sendResponse = require("../utlites/sendResponse");
 
 exports.getUser = async (req, res) => {
-    const email = req.query.email;
-    const query = { email: email }
-    // console.log(query);
-    // const result = await usersCollection.findOne(query);
-    res.send("got it user by ")
+    try {
+        const Db = getDB();
+        const email = req.query.email;
+        const query = { email: email }
+        const result = await Db.collection('usersCollection').findOne(query);
+        sendResponse(res, result)
+    } catch (err) {
+        res.status(500).send({ status: "error", message: err.message });
+    }
 }

7ab5ec3e292f49de367b7b4d0fb9849fcc6e72d2
raihanuldev <rihanthedev@gmail.com>
2025-12-05 12:37:40 +0600

    Logger Middileware added

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+
+
 // Routes
 const userRoutes = require('./src/routes/user.routes');
+const logger = require('./src/middilwares/Logger');
 
+app.use(logger)
 app.use('/v1/user',userRoutes)
 
 app.listen(port, '0.0.0.0', () => {

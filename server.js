29d70d2f879b31291215eb920cf33d97ffde7305
raihanuldev <rihanthedev@gmail.com>
2025-12-08 00:37:57 +0600

    fixed build isses

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { connectDb } = require('./src/config/db')
 const PORT = process.env.PORT || 5000
 
 connectDb().then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Local Server running on port ${PORT}`));
 }).catch(err => console.log('DB connection failed:', err));
 
 

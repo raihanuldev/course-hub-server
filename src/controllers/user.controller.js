da785c2f69d121b802e0439e8956936ea5a130ee
raihanuldev <rihanthedev@gmail.com>
2025-12-08 00:02:45 +0600

    feat;ClubReletedAPIS

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const { getDB } = require("../config/db");
 const sendResponse = require("../utlites/sendResponse");
-const {ObjectId} = require('mongodb')
+const { ObjectId } = require('mongodb')
 
 exports.getAllUser = async (req, res) => {
     try {
@@ -67,11 +67,61 @@ exports.postUser = async (req, res) => {
 }
 
 exports.MakeAdmin = async (req, res) => {
+    const Db = getDB();
     const id = req.params.id;
     const _id = new ObjectId(id)
-    const result = await usersCollection.findOneAndUpdate(
+    const result = await Db.collection('usersCollection').findOneAndUpdate(
         { _id: _id },
         { $set: { role: 'admin' } }
     )
     res.send(result)
+}
+exports.isClubMember = async (req, res) => {
+    const db = getDB();
+    const email = req.query.email;
+    try {
+        const member = await db.collection('clubMemberCollection').findOne({ email });
+        res.send({ isMember: !!member });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ isMember: false });
+    }
+}
+exports.GetAllClubMembers = async (req, res) => {
+    const db = getDB();
+    try {
+        const members = await db.collection('clubMemberCollection').find().toArray();
+        res.send(members);
+    } catch (error) {
+        console.error("Failed to fetch club members:", error);
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
+}
+
+exports.addClubMember = async (req, res) => {
+    const db = getDB();
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).send({ message: "Email is required" });
+    }
+
+    try {
+        // Check if already exists
+        const existing = await db.collection('clubMemberCollection').findOne({ email });
+        if (existing) {
+            return res.status(200).send({ message: "Already a club member" });
+        }
+
+        // Insert new member
+        const result = await clubMemberCollection.insertOne({
+            email,
+            role: "member"
+        });
+
+        res.status(201).send({ message: "Added to club members", insertedId: result.insertedId });
+    } catch (error) {
+        console.error("Failed to add club member:", error);
+        res.status(500).send({ message: "Server Error" });
+    }
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getUser, postUser, getAllUser, MakeAdmin } = require('../controllers/user.controller');
+const { getUser, postUser, getAllUser, MakeAdmin, isClubMember, GetAllClubMembers, addClubMember } = require('../controllers/user.controller');
 
 const router = express.Router();
 
@@ -7,4 +7,8 @@ router.get('/getUser',getUser)
 router.post('/postuser',postUser)
 router.get('/getAllUsers',getAllUser)
 router.put('/make-admin/:id',MakeAdmin)
+router.get('/is-club-member',isClubMember)
+router.get('/club-members',GetAllClubMembers)
+router.post('/add-club-member',addClubMember)
+
 module.exports= router;

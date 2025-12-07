b2e5fcd93d674504bf65026eced6bc4af396dd16
raihanuldev <rihanthedev@gmail.com>
2025-12-08 00:06:24 +0600

    convert single pattern to MVC

--- index.js
+++
@@ -1,6 +0,0 @@
-const couresCollection = client.db('Language').collection('couresCollection');
-const usersCollection = client.db('Language').collection('usersCollection');
-const cartCollection = client.db('Language').collection('cartCollection');
-const paymentCollection = client.db('Language').collection('paymentCollection');
-const contentCollection = client.db('Language').collection('content-collections');
-const clubMemberCollection = client.db('Language').collection('clubMemberCollection');

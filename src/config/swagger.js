00ce0db2f3d214dfebe1eb304436963a508a896f
raihanuldev <rihanthedev@gmail.com>
2025-12-25 00:41:07 +0600

    added

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:5000",  // local
+        url: "http://localhost:8080",  // local
         description: "Local Server"
       },
       {
